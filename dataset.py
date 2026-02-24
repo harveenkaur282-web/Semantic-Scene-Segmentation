7d7676f9519ac5c155d668476474c79b7cfd82ca
Harveen Kaur <harveenkaur282@gmail.com>
2026-02-24 04:02:24 +0000

    Setup dataset, model with 10 classes, basic train pipeline working

---
+++ dataset.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import numpy as np
+
+class SegmentationDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+
+        self.images = sorted(os.listdir(image_dir))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.images[index])
+
+        image = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path)
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        mask = torch.as_tensor(
+            np.array(mask),
+            dtype=torch.long
+        )
+
+        return image, mask
---
+++ train.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
+from dataset import SegmentationDataset
+from models.deeplab import get_model   # adjust if path slightly different
+
+# ------------------------
+# Config
+# ------------------------
+num_classes = 10
+batch_size = 4
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+# ------------------------
+# Dataset
+# ------------------------
+train_image_dir = "dataset/Offroad_Segmentation_Training_Dataset/train/Color_Images"
+train_mask_dir = "dataset/Offroad_Segmentation_Training_Dataset/train/Segmentation"
+
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+train_dataset = SegmentationDataset(
+    image_dir=train_image_dir,
+    mask_dir=train_mask_dir,
+    transform=transform
+)
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+# ------------------------
+# Model
+# ------------------------
+model = get_model(num_classes)
+model = model.to(device)
+
+print("Model loaded successfully.")
