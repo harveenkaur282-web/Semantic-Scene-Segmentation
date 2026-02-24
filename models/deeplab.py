547b56a1193efbe5218fce176f18f52391476088
Harveen Kaur <harveenkaur282@gmail.com>
2026-02-24 02:30:36 +0000

    added deeplab model setup

---
+++ models/deeplab.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+
+def get_model(num_classes):
+    model = torchvision.models.segmentation.deeplabv3_resnet50(
+        weights="DEFAULT"
+    )
+
+    model.aux_classifier[4] = nn.Conv2d(
+        in_channels=256,
+        out_channels=num_classes,
+        kernel_size=1
+    )
+
+    return model
